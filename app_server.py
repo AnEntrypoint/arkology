4f148df743e7d5c116511c6efb64bf59dc69988d
Setup <test@example.com>
2026-02-24 08:52:15 +0000

    Fix Traefik bad gateway by binding Flask to 0.0.0.0

--- app_server.py
+++ app_server.py
@@ -145,4 +145,4 @@ def get_crime_descriptions():
     return jsonify(load('crime_descriptions'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    app.run(host='0.0.0.0', port=3000)
