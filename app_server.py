2946536b32657c7301e9df45fff84dee08da4ec4
Setup <test@example.com>
2026-02-24 08:40:04 +0000

    Move Flask server from port 5000 to port 3000

--- app_server.py
+++ app_server.py
@@ -145,4 +145,4 @@ def get_crime_descriptions():
     return jsonify(load('crime_descriptions'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=3000)
